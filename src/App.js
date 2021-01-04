41c5c789bd4890ba7da4c93417502b8026832cc5
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-04 15:26:10 -0500

    Optimize imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/jsx-filename-extension */
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
