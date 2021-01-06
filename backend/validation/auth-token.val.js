3bf3b9d1916c1f48b6db8248a593fe0d9462b2e5
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-06 10:05:44 -0500

    Make a middleware that checks if an user is authenticated

--- backend/validation/auth-token.val.js
+++ backend/validation/auth-token.val.js
@@ -1,1 +1,16 @@
-// TODO: Make a middleware that checks if an user is authenticated
+
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const token = req.header('auth-token');
+  if (!token) return res.status(401).send({ error: 'Access Denied' });
+
+  try {
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.userId = verified;
+    next();
+    return res.status(200);
+  } catch (err) {
+    return res.status(400).send({ error: 'Invalid token' });
+  }
+};
