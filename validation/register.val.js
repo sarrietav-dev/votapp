6b04f367cf74d1122290fd25362aca3302c490b3
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-06 12:33:48 -0500

    Change username min from 6 to 4 and remove requirements

--- validation/register.val.js
+++ validation/register.val.js
@@ -2,7 +2,7 @@ const Joi = require('@hapi/joi');
 
 const registerValidation = (data) => {
   const schema = Joi.object({
-    name: Joi.string().min(6).max(255).required(),
+    name: Joi.string().min(4).max(255).required(),
     password: Joi.string().min(6).max(1024).required(),
     email: Joi.string().min(6).max(1024).required().email(),
     gender: Joi.boolean().required(),
