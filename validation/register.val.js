45504b183e1cf6fe0261920841565b1239f81fb3
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-06 00:46:46 -0500

    Validate inside function

--- validation/register.val.js
+++ validation/register.val.js
@@ -1,13 +1,16 @@
 const Joi = require('@hapi/joi');
 
-// Validation Schema
-const schema = Joi.object({
-  name: Joi.string().min(6).max(255).required(),
-  password: Joi.string().min(6).max(1024).required(),
-  email: Joi.string().min(6).max(1024).required().email(),
-  gender: Joi.boolean().required(),
-  phoneNumber: Joi.string().max(10),
-  birthday: Joi.date(),
-});
+const registerValidation = (data) => {
+  const schema = Joi.object({
+    name: Joi.string().min(6).max(255).required(),
+    password: Joi.string().min(6).max(1024).required(),
+    email: Joi.string().min(6).max(1024).required().email(),
+    gender: Joi.boolean().required(),
+    phoneNumber: Joi.string().max(10),
+    birthday: Joi.date(),
+  });
 
-module.exports = schema;
+  return schema.validate(data);
+};
+
+module.exports = registerValidation;
