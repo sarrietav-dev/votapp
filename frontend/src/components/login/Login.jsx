d85165659933d489df110f02f318085a5718b3cf
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-06 13:52:18 -0500

    Create api call on login

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 // eslint-disable-next-line object-curly-newline
 import { Button, Container, Grid, makeStyles } from '@material-ui/core';
+import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -9,13 +10,26 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const loginUser = async (credentials) => {
+  const response = await axios({
+    method: 'POST',
+    url: 'http://localhost:5000/api/login',
+    data: credentials,
+  });
+  return response.data.headers['auth-token'];
+};
+
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const classes = useStyles();
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const token = loginUser({ email, password });
+  };
 
   return (
     <Container className={classes.container} maxWidth="md">
