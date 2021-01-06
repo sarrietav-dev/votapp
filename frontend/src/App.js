be9dc6f8ccfe72bfed0bfbc0be1456542d99e73e
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-06 16:40:29 -0500

    Add token state to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/jsx-filename-extension */
+import { useState } from 'react';
 import './App.css';
 import Login from './components/login/Login';
 
 function App() {
+  const [token, setToken] = useState();
+
+  const saveToken = (tokenArg) => {
+    setToken(tokenArg);
+  };
+
   return (
     <div className="App">
-      <Login />
+      <Login setToken={saveToken} />
+      {token}
     </div>
   );
 }
