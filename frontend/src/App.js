8dbd202ddde662d1641cd28fca4191c57397cc91
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-06 16:55:38 -0500

    Create setToken function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,17 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/jsx-filename-extension */
-import { useState } from 'react';
 import './App.css';
 import Login from './components/login/Login';
 
 function App() {
-  const [token, setToken] = useState();
+  const setToken = (token) => token;
 
-  const saveToken = (tokenArg) => {
-    setToken(tokenArg);
-  };
+  const token = setToken();
 
   return (
     <div className="App">
-      <Login setToken={saveToken} />
+      <Login setToken={token} />
       {token}
     </div>
   );
