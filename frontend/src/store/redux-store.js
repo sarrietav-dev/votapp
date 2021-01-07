b14326046fcb518bf18247076e196180bf5a1eab
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-07 17:33:29 -0500

    Add root reducer and redux devtools to store

--- frontend/src/store/redux-store.js
+++ frontend/src/store/redux-store.js
@@ -1,3 +1,9 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-underscore-dangle */
 import { createStore } from 'redux';
+import rootReducer from '../reducers/root-reducer';
 
-export default createStore();
+export default createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
