77edd99ad1d597a0fbfcc1df884bb74fe4b059e9
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-07 00:23:39 -0500

    Add styles and new icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
-// eslint-disable-next-line object-curly-newline
-import { Toolbar, AppBar, Typography, IconButton } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
+import {
+  Toolbar,
+  AppBar,
+  Typography,
+  IconButton,
+  makeStyles,
+} from '@material-ui/core';
+import HowToVoteIcon from '@material-ui/icons/HowToVote';
+import SettingsIcon from '@material-ui/icons/Settings';
+
+const useStyles = makeStyles((theme) => ({
+  navbarSettings: {
+    marginLeft: 'auto',
+  },
+}));
 
 const Navbar = () => (
   <div className="appbar-wrapper">
     <AppBar position="static">
       <Toolbar>
         <IconButton edge="start" color="inherit">
-          <MenuIcon />
+          <HowToVoteIcon />
         </IconButton>
         <Typography variant="h5">Voteapp</Typography>
+        <IconButton color="inherit">
+          <SettingsIcon />
+        </IconButton>
       </Toolbar>
     </AppBar>
   </div>
