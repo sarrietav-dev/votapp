0115ab607937d4476af3f5b4caeb4cfec6be4ac1
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-07 18:02:46 -0500

    Create auth reducer

--- frontend/src/reducers/reducer.js
+++ frontend/src/reducers/reducer.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import authReducer from './auth-token.reducer';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  authReducer,
+});
 
 export default rootReducer;
