7be1fbaa10043d70f90f83dcf3272e3600d811fc
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-07 17:19:27 -0500

    Add provider to app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
 
