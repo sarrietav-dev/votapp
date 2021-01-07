6afa47f35f673add6503a888cc0b47c810143bad
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-06 23:53:05 -0500

    FIX: Import error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 // eslint-disable-next-line object-curly-newline
 import { Toolbar, AppBar, Typography, IconButton } from '@material-ui/core';
-import { MenuIcon } from '@material-ui/icons/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const Navbar = () => (
   <div className="appbar-wrapper">
     <AppBar position="static">
       <Toolbar>
-        <IconButton edge="start">
+        <IconButton edge="start" color="inherit">
           <MenuIcon />
         </IconButton>
+        <Typography variant="h5">Voteapp</Typography>
       </Toolbar>
-      <Typography variant="h4">Voteapp</Typography>
     </AppBar>
   </div>
 );
