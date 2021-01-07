bad44270ea6a7fc9063df9c2d5294c9aa7a9439e
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-07 17:36:55 -0500

    Rename redux-store to store

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-import store from './store/redux-store';
+import store from './store/store';
 
 ReactDOM.render(
   <Provider store={store}>
