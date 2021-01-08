e68ea1146c1b7a44deedcf608da790c4dba14bee
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-07 23:37:26 -0500

    Use styles on FAB button

--- src/components/dashboard/FabButton.jsx
+++ src/components/dashboard/FabButton.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { Fab } from '@material-ui/core';
+import { Fab, makeStyles } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 
-const FabButton = () => (
-  <Fab color="secondary" aria-label="add">
-    <AddIcon />
-  </Fab>
-);
+const useStyles = makeStyles(() => ({
+  fab: {
+    margin: 0,
+    top: 'auto',
+    right: 20,
+    bottom: 20,
+    left: 'auto',
+    position: 'fixed',
+  },
+}));
+
+const FabButton = () => {
+  const classes = useStyles();
+
+  return (
+    <Fab color="secondary" aria-label="add" className={classes.fab}>
+      <AddIcon />
+    </Fab>
+  );
+};
 
 export default FabButton;
