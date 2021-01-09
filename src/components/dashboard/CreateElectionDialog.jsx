d7a799dddc87da3e832cb8cc4c18b02f8b37fe69
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-08 23:35:23 -0500

    Create CreateElectionDialog component

---
+++ src/components/dashboard/CreateElectionDialog.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+} from '@material-ui/core';
+
+const CreateElectionDialog = ({ open, onClose }) => (
+  <Dialog open={open} onClose={onClose}>
+    <DialogTitle>Create election</DialogTitle>
+    <DialogContent>
+      <TextField
+        autoFocus
+        margin="dense"
+        id="election-title"
+        label="Title"
+        type="text"
+        fullWidth
+        variant="filled"
+      />
+    </DialogContent>
+    <DialogActions>
+      <Button color="primary">Cancel</Button>
+      <Button color="primary">Create</Button>
+    </DialogActions>
+  </Dialog>
+);
+
+CreateElectionDialog.propTypes = {
+  open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default CreateElectionDialog;
