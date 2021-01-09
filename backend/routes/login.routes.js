19cca4ad8d56858b83e5b03c23916a715a6ff51b
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-09 14:24:53 -0500

    FIX: import

--- backend/routes/login.routes.js
+++ backend/routes/login.routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/User.model');
 const loginValidation = require('../validation/login.val');
 
 router.post('/', async (req, res) => {
