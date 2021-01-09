ec88917108a5ff556cdda24ef7a5ffae257c4f3d
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-09 00:38:56 -0500

    Add fabReducer to root reducer

--- frontend/src/reducers/fab.reducer.js
+++ frontend/src/reducers/fab.reducer.js
@@ -4,17 +4,21 @@ const defaultState = {
   isOpen: false,
 };
 
-export default (state = defaultState, action) => {
+const fabReducer = (state = defaultState, action) => {
   switch (action.type) {
     case OPEN_DIALOG:
       return {
+        ...state,
         isOpen: true,
       };
     case CLOSE_DIALOG:
       return {
+        ...state,
         isOpen: false,
       };
     default:
       return state;
   }
 };
+
+export default fabReducer;
--- frontend/src/reducers/reducer.js
+++ frontend/src/reducers/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './auth-token.reducer';
+import fabReducer from './fab.reducer';
 
 const rootReducer = combineReducers({
   authReducer,
+  fabReducer,
 });
 
 export default rootReducer;
