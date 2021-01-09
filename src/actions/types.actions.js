dc897282e99f10b6b18e0ff9d12ddb6f42ede203
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-08 23:54:21 -0500

    Create fab dialog actions

--- src/actions/types.actions.js
+++ src/actions/types.actions.js
@@ -1,4 +1,3 @@
-const SET_AUTH_TOKEN = 'SET_AUTH_TOKEN';
-const SET_TEST = '';
-
-export { SET_AUTH_TOKEN, SET_TEST };
+export const SET_AUTH_TOKEN = 'SET_AUTH_TOKEN';
+export const CLOSE_DIALOG = 'CLOSE_DIALOG';
+export const OPEN_DIALOG = 'OPEN_DIALOG';
