4162f2187204148de403b3a458fb14fda4c0b38e
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-08 23:57:54 -0500

    Add onClick event to FAB

--- frontend/src/components/dashboard/FabButton.jsx
+++ frontend/src/components/dashboard/FabButton.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Fab, makeStyles } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
+import useDispatch from 'react-redux';
 import CreateElectionDialog from './CreateElectionDialog';
+import { openDialog } from '../../actions/fab.actions';
 
 const useStyles = makeStyles(() => ({
   fab: {
@@ -16,16 +18,20 @@ const useStyles = makeStyles(() => ({
 
 const FabButton = () => {
   const classes = useStyles();
-
-  const handleClick = () => {};
-
-  const handleClose = () => {};
+  const dispatch = useDispatch();
 
   return (
-    <Fab color="secondary" aria-label="add" className={classes.fab} onClick={handleClick}>
-      <AddIcon />
-    </Fab>
-    <CreateElectionDialog />
+    <div className="fab-wrapper">
+      <Fab
+        color="secondary"
+        aria-label="add"
+        className={classes.fab}
+        onClick={() => dispatch(openDialog())}
+      >
+        <AddIcon />
+      </Fab>
+      <CreateElectionDialog />
+    </div>
   );
 };
 
