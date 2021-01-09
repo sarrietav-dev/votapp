a0d02e84772e4c724ff60edf276480da1679d969
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-09 15:53:06 -0500

    Save election to cloud database

--- routes/election.routes.js
+++ routes/election.routes.js
@@ -2,9 +2,21 @@ const router = require('express').Router();
 const Election = require('../models/Election.model');
 const electionValidation = require('../validation/election.val');
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const { error } = electionValidation(req.body);
   if (error) return res.status(400).json({ error: error.details[0].message });
+
+  const election = new Election({
+    title: req.body.title,
+    position: req.body.position,
+  });
+
+  try {
+    await election.save();
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(400).json({ err });
+  }
 });
 
 module.exports = router;
