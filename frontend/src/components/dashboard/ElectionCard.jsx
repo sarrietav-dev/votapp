6fbfd2751aa961b385ac5e1e33f0e889dc0ce5e0
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-10 00:32:43 -0500

    Styled Election Card

--- frontend/src/components/dashboard/ElectionCard.jsx
+++ frontend/src/components/dashboard/ElectionCard.jsx
@@ -5,29 +5,42 @@ import {
   CardActions,
   CardContent,
   IconButton,
+  makeStyles,
   Typography,
 } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
 
-const ElectionCard = () => (
-  <Card>
-    <CardActionArea>
-      <CardContent>
-        <Typography>Election</Typography>
-        <Typography>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nulla,
-          ullam numquam minima architecto maiores similique, atque voluptas
-          eveniet, deserunt possimus dolores doloribus incidunt provident in
-          quo? Nostrum, autem velit!
-        </Typography>
-      </CardContent>
-    </CardActionArea>
-    <CardActions>
-      <IconButton>
-        <EditIcon />
-      </IconButton>
-    </CardActions>
-  </Card>
-);
+const useStyles = makeStyles({
+  card: {
+    width: 275,
+  },
+  editButton: {
+    marginLeft: 'auto',
+  },
+});
+
+const ElectionCard = () => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card} variant="outlined">
+      <CardActionArea>
+        <CardContent>
+          <Typography>Election</Typography>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            nulla, ullam numquam minima architecto maiores similique, atque
+            voluptas
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <IconButton className={classes.editButton}>
+          <EditIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
 
 export default ElectionCard;
