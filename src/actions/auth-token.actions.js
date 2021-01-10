a037bf100abec6cd3a4be249dab48e8c34f0ec61
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-10 18:55:25 -0500

    Delete useless test action

--- src/actions/auth-token.actions.js
+++ src/actions/auth-token.actions.js
@@ -1,8 +1,6 @@
 import { SET_AUTH_TOKEN } from './types.actions';
 
-export const setAuthToken = (token) => ({
+export default (token) => ({
   type: SET_AUTH_TOKEN,
   payload: token,
 });
-
-export const setTest = () => ({});
