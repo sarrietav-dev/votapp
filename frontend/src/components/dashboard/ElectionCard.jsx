1b508cd358036ea48748cd28c533b9c317e9e6a5
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-10 00:07:19 -0500

    Define Election Card body

--- frontend/src/components/dashboard/ElectionCard.jsx
+++ frontend/src/components/dashboard/ElectionCard.jsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  IconButton,
+  Typography,
+} from '@material-ui/core';
+import EditIcon from '@material-ui/icons/Edit';
 
-const ElectionCard = () => {};
+const ElectionCard = () => {
+  <Card>
+    <CardActionArea>
+      <CardContent>
+        <Typography>Election</Typography>
+        <Typography>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nulla,
+          ullam numquam minima architecto maiores similique, atque voluptas
+          eveniet, deserunt possimus dolores doloribus incidunt provident in
+          quo? Nostrum, autem velit!
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+    <CardActions>
+      <IconButton>
+        <EditIcon />
+      </IconButton>
+    </CardActions>
+  </Card>;
+};
 
 export default ElectionCard;
