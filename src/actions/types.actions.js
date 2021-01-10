7167b03e3efb913aa37fec6de3ee835c91b0bac3
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-10 18:51:04 -0500

    Create actions to handle election state

--- src/actions/types.actions.js
+++ src/actions/types.actions.js
@@ -1,3 +1,5 @@
 export const SET_AUTH_TOKEN = 'SET_AUTH_TOKEN';
 export const CLOSE_DIALOG = 'CLOSE_DIALOG';
 export const OPEN_DIALOG = 'OPEN_DIALOG';
+export const SET_ELECTIONS = 'SET_ELECTIONS';
+export const GET_ELECTIONS = 'GET_ELECTIONS';
