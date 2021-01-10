7144d812337681bb6c19588e629cc012693c6c2c
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-10 00:40:58 -0500

    Add TODO

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -4,6 +4,8 @@ import CreateElectionDialog from './CreateElectionDialog';
 import ElectionCard from './ElectionCard';
 import FabButton from './FabButton';
 
+// TODO: Map Election Cards.
+
 const Dashboard = () => (
   <div className="dashboard-wrapper">
     <Navbar />
