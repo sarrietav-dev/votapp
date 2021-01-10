bdab126663233591284cd2c4b2aa1aa2f3f040a5
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-10 00:09:24 -0500

    Add Election Card to Dashboard

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../Navbar';
 import CreateElectionDialog from './CreateElectionDialog';
+import ElectionCard from './ElectionCard';
 import FabButton from './FabButton';
 
 const Dashboard = () => (
   <div className="dashboard-wrapper">
     <Navbar />
+    <ElectionCard />
     <FabButton />
     <CreateElectionDialog />
   </div>
