34bbb1517fd75621d4be6b2fbf688848df174812
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-11 16:11:59 -0500

    Rename setOneElection action to saveElection

--- frontend/src/store/actions/election.actions.js
+++ frontend/src/store/actions/election.actions.js
@@ -1,11 +1,11 @@
-import { FETCH_ELECTIONS, SET_ONE_ELECTION } from './types.actions';
+import { FETCH_ELECTIONS, SAVE_ELECTION } from './types.actions';
 
 export const fetchElections = (data) => ({
   type: FETCH_ELECTIONS,
   payload: data,
 });
 
-export const setOneElection = (data) => ({
-  type: SET_ONE_ELECTION,
+export const saveElection = (data) => ({
+  type: SAVE_ELECTION,
   payload: data,
 });
