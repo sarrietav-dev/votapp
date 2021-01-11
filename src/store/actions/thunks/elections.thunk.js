b8d8e728caf133267b2f679367cdacc658d5bee1
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-11 15:05:51 -0500

    Add API call on thunk

--- src/store/actions/thunks/elections.thunk.js
+++ src/store/actions/thunks/elections.thunk.js
@@ -1,3 +1,12 @@
-export const getAllElections = () => () => {};
+import axios from 'axios';
+import { setElections } from '../election.actions';
+
+export const getAllElections = () => async (dispatch) => {
+  await axios
+    .get('http://localhost:5000/api/elections/')
+    .then((response) => dispatch(setElections(response.data)))
+    // eslint-disable-next-line no-console
+    .catch((err) => console.log(err));
+};
 
 export const saveElection = () => () => {};
