e78a9c15defd822bdd047a078426a2982e405d80
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-10 23:59:19 -0500

    Minor: Import UUID and use it on Dashboard map

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { v4 as uuid } from 'uuid';
 import Navbar from '../Navbar';
 import CreateElectionDialog from './CreateElectionDialog';
 import ElectionCard from './ElectionCard';
@@ -25,7 +26,11 @@ const Dashboard = () => {
       <FabButton />
       <CreateElectionDialog />
       {elections.map((election) => (
-        <ElectionCard title={election.title} position={election.position} />
+        <ElectionCard
+          title={election.title}
+          position={election.position}
+          key={uuid()}
+        />
       ))}
     </div>
   );
