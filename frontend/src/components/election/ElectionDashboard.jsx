a07f46799cac1957c776ee5b1aefe68faa238501
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-18 15:16:45 -0500

    Add Settings button

--- frontend/src/components/election/ElectionDashboard.jsx
+++ frontend/src/components/election/ElectionDashboard.jsx
@@ -1,5 +1,6 @@
-import { Button } from '@material-ui/core';
+import { Button, IconButton } from '@material-ui/core';
 import React from 'react';
+import SettingsIcon from '@material-ui/icons/Settings';
 import FabButton from '../FabButton';
 import NavBar from '../Navbar';
 
@@ -9,7 +10,12 @@ const ElectionDashboard = () => (
     <FabButton />
     <div className="banner">
       <h1>Title</h1>
-      <Button color="secondary" variant="contained">Action button</Button>
+      <Button color="secondary" variant="contained">
+        Action button
+      </Button>
+      <IconButton>
+        <SettingsIcon />
+      </IconButton>
     </div>
   </div>
 );
