1eb6bbd3844b1d1e8f1e7f432875b730b668ad10
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-18 15:27:43 -0500

    Add styles to component

--- frontend/src/components/election/ElectionDashboard.jsx
+++ frontend/src/components/election/ElectionDashboard.jsx
@@ -1,23 +1,31 @@
-import { Button, IconButton } from '@material-ui/core';
+import { Button, IconButton, makeStyles } from '@material-ui/core';
 import React from 'react';
 import SettingsIcon from '@material-ui/icons/Settings';
 import FabButton from '../FabButton';
 import NavBar from '../Navbar';
 
-const ElectionDashboard = () => (
-  <div className="electionWrapper">
-    <NavBar actionIcon="back" path={{ from: '/' }} />
-    <FabButton />
-    <div className="banner">
-      <h1>Title</h1>
-      <Button color="secondary" variant="contained">
-        Action button
-      </Button>
-      <IconButton>
-        <SettingsIcon />
-      </IconButton>
+const useStyles = makeStyles(() => ({
+  electionWrapper: {},
+  banner: {},
+}));
+
+const ElectionDashboard = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.electionWrapper}>
+      <NavBar actionIcon="back" path={{ from: '/' }} />
+      <FabButton />
+      <div className={classes.banner}>
+        <h1>Title</h1>
+        <Button color="secondary" variant="contained">
+          Action button
+        </Button>
+        <IconButton>
+          <SettingsIcon />
+        </IconButton>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ElectionDashboard;
