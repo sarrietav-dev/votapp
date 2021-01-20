2b5d6be5d7c48088032a1ced9084038f42138a76
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-20 15:14:23 -0500

    Implement case on election reducer for SET_CURRENT_ELECTION

--- frontend/src/store/reducers/elections.reducer.js
+++ frontend/src/store/reducers/elections.reducer.js
@@ -1,7 +1,13 @@
-import { FETCH_ELECTIONS, SAVE_ELECTION } from '../actions/types.actions';
+/* eslint-disable no-underscore-dangle */
+import {
+  FETCH_ELECTIONS,
+  SAVE_ELECTION,
+  SET_CURRENT_ELECTION,
+} from '../actions/types.actions';
 
 const defaultState = {
   elections: [],
+  currentElection: {},
 };
 
 export default (state = defaultState, action) => {
@@ -13,6 +19,13 @@ export default (state = defaultState, action) => {
     case SAVE_ELECTION:
       return {
         elections: [...state.elections, action.payload],
+      };
+    case SET_CURRENT_ELECTION:
+      return {
+        ...state,
+        currentElection: state.elections.filter(
+          (election) => election._id === action.payload,
+        ),
       };
     default:
       return state;
