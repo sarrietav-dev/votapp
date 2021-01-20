79a74ea5eca935560519bb27005c33aa8b9a86ce
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-20 17:25:14 -0500

    Use redux to handle open and close state

--- src/components/election/DeleteElectionWarning.jsx
+++ src/components/election/DeleteElectionWarning.jsx
@@ -6,16 +6,23 @@ import {
   DialogTitle,
 } from '@material-ui/core';
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { closeDialog } from '../../store/actions/dialog.actions';
 
-const DeleteElectionWarning = () => (
-  <Dialog>
-    <DialogTitle>Are you sure you want to delete this?</DialogTitle>
-    <DialogContent>This can&apost be undone</DialogContent>
-    <DialogActions>
-      <Button>Cancel</Button>
-      <Button>Continue</Button>
-    </DialogActions>
-  </Dialog>
-);
+const DeleteElectionWarning = () => {
+  const isOpen = useSelector((state) => state.dialog.isOpen);
+  const dispatch = useDispatch();
+
+  return (
+    <Dialog open={isOpen}>
+      <DialogTitle>Are you sure you want to delete this?</DialogTitle>
+      <DialogContent>This can&apost be undone</DialogContent>
+      <DialogActions>
+        <Button onClick={() => dispatch(closeDialog())}>Cancel</Button>
+        <Button>Continue</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
 
 export default DeleteElectionWarning;
