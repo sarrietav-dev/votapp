1c85f3837cf3082b2bf1c2e857ed433193bfbddf
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-20 14:25:47 -0500

    Add form and supervise form fields

--- frontend/src/components/election/ElectionSettings.jsx
+++ frontend/src/components/election/ElectionSettings.jsx
@@ -6,34 +6,41 @@ import {
   DialogTitle,
   TextField,
 } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 const ElectionSettings = () => {
+  const [title, setTitle] = useState('');
+  const [position, setPosition] = useState('');
   const isOpen = useSelector((state) => state.dialog.isOpen);
+
   return (
     <Dialog open={isOpen}>
-      <DialogTitle>Election Title</DialogTitle>
-      <DialogContent>
-        <TextField
-          margin="dense"
-          label="Title"
-          type="text"
-          fullWidth
-          variant="filled"
-        />
-        <TextField
-          margin="dense"
-          label="Position"
-          type="text"
-          fullWidth
-          variant="filled"
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button color="primary">Cancel</Button>
-        <Button color="primary">Accept</Button>
-      </DialogActions>
+      <form>
+        <DialogTitle>Election Title</DialogTitle>
+        <DialogContent>
+          <TextField
+            margin="dense"
+            label="Title"
+            type="text"
+            fullWidth
+            variant="filled"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <TextField
+            margin="dense"
+            label="Position"
+            type="text"
+            fullWidth
+            variant="filled"
+            onChange={(e) => setPosition(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button color="primary">Cancel</Button>
+          <Button color="primary">Accept</Button>
+        </DialogActions>
+      </form>
     </Dialog>
   );
 };
