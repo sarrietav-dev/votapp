453acee79c0be176e400cb6deb4051bc13ca2981
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-20 14:13:20 -0500

    Use dialog redux state on ElectionSettings

--- frontend/src/components/election/ElectionSettings.jsx
+++ frontend/src/components/election/ElectionSettings.jsx
@@ -7,31 +7,35 @@ import {
   TextField,
 } from '@material-ui/core';
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const ElectionSettings = () => (
-  <Dialog>
-    <DialogTitle>Election Title</DialogTitle>
-    <DialogContent>
-      <TextField
-        margin="dense"
-        label="Title"
-        type="text"
-        fullWidth
-        variant="filled"
-      />
-      <TextField
-        margin="dense"
-        label="Position"
-        type="text"
-        fullWidth
-        variant="filled"
-      />
-    </DialogContent>
-    <DialogActions>
-      <Button color="primary">Cancel</Button>
-      <Button color="primary">Accept</Button>
-    </DialogActions>
-  </Dialog>
-);
+const ElectionSettings = () => {
+  const isOpen = useSelector((state) => state.dialog.isOpen);
+  return (
+    <Dialog open={isOpen}>
+      <DialogTitle>Election Title</DialogTitle>
+      <DialogContent>
+        <TextField
+          margin="dense"
+          label="Title"
+          type="text"
+          fullWidth
+          variant="filled"
+        />
+        <TextField
+          margin="dense"
+          label="Position"
+          type="text"
+          fullWidth
+          variant="filled"
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button color="primary">Cancel</Button>
+        <Button color="primary">Accept</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
 
 export default ElectionSettings;
