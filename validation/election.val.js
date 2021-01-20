26246afdb9707e5ac4758e7b620ccedc1b8ef1a1
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-20 16:45:44 -0500

    Add more fields to electionValidation

--- validation/election.val.js
+++ validation/election.val.js
@@ -2,8 +2,12 @@ const Joi = require('@hapi/joi');
 
 const electionValidation = (data) => {
   const schema = Joi.object({
+    _id: Joi.string(),
     title: Joi.string().min(5).required(),
     position: Joi.string().min(5).required(),
+    registeredVotes: Joi.array(),
+    votes: Joi.array(),
+    __v: Joi.number(),
   });
 
   return schema.validate(data);
