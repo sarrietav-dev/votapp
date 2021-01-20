807124e615e0263afa9e535cfc90fe1a3f7b54a2
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-20 14:51:51 -0500

    Add comments to acton types

--- src/store/actions/types.actions.js
+++ src/store/actions/types.actions.js
@@ -1,6 +1,9 @@
+// Authentication actions.
 export const SET_AUTH_TOKEN = 'SET_AUTH_TOKEN';
+export const LOG_OUT = 'LOG_OUT';
+// Dialog actions.
 export const CLOSE_DIALOG = 'CLOSE_DIALOG';
 export const OPEN_DIALOG = 'OPEN_DIALOG';
+// Elections actions.
 export const FETCH_ELECTIONS = 'FETCH_ELECTIONS';
 export const SAVE_ELECTION = 'SAVE_ELECTION';
-export const LOG_OUT = 'LOG_OUT';
