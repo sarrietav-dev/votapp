91998d059257d39709deeffa58a332161bbe36c4
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-22 17:45:47 -0500

    feat(database): replace db mocking with mongodb-memory-server
    Also, we changed promises to async/await functions

--- backend/database/index.js
+++ backend/database/index.js
@@ -2,46 +2,51 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const chalk = require('chalk');
+const { MongoMemoryServer } = require('mongodb-memory-server');
 
 const { log } = console;
+const mongod = new MongoMemoryServer();
 
 dotenv.config();
 
-function connect() {
-  return new Promise((resolve, reject) => {
-    if (process.env.NODE_ENV === 'test') {
-      const Mockgoose = require('mockgoose').Mockgoose;
-      const mockgoose = new Mockgoose(mongoose);
+module.exports.connect = async () => {
+  if (process.env.NODE_ENV === 'test') {
+    const URI = await mongod.getUri();
 
-      mockgoose.prepareStorage().then(() => {
-        mongoose
-          .connect(process.env.CONNECTION_STRING, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-          })
-          .then((res, err) => {
-            if (err) return reject(err);
-            log(chalk.blue('Connected to MongoDB Atlas!'));
-            resolve();
-          });
-      });
-    } else {
-      mongoose
-        .connect(process.env.CONNECTION_STRING, {
-          useNewUrlParser: true,
-          useCreateIndex: true,
-        })
-        .then((res, err) => {
-          if (err) return reject(err);
-          log(chalk.blue('Connected to MongoDB Atlas!'));
-          resolve();
-        });
-    }
-  });
-}
+    await mongoose(URI, {
+      useNewUrlParser: true,
+      autoReconnect: true,
+      reconnectTries: Number.MAX_VALUE,
+      reconnectInterval: 1000,
+    })
+      .then((res) => {
+        log(chalk.blue('Connected to MongoDB Atlas!'));
+      })
+      .catch((err) => log(chalk.red(err)));
+  } else {
+    await mongoose
+      .connect(process.env.CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      })
+      .then((res) => {
+        log(chalk.blue('Connected to MongoDB Atlas!'));
+      })
+      .catch((err) => log(chalk.red(err)));
+  }
+};
 
-function close() {
-  return mongoose.disconnect();
-}
+module.exports.close = async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongod.stop();
+};
 
-module.exports = { connect, close };
+module.exports.clear = async () => {
+  const collections = mongoose.connection.collections;
+
+  for (const key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany();
+  }
+};
