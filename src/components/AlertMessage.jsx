7f768a6e6837e63a14e7c3035afae0012972197a
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-21 23:53:00 -0500

    refactor(alertmessage): delete unused import
    PropTypes import was deleted
    
    BREAKING CHANGE: The variable type, which was a prop, still exists

--- src/components/AlertMessage.jsx
+++ src/components/AlertMessage.jsx
@@ -1,7 +1,6 @@
 import { Snackbar } from '@material-ui/core';
 import Alert from '@material-ui/lab/Alert';
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const AlertMessage = () => (
   <Snackbar autoHideDuration={6000}>
