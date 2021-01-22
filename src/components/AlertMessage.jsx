bd59fd71fac48b61d62dd91b0fedc20212dadcd7
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-21 23:51:15 -0500

    refactor(alertmessage): delete props and proptypes
    These are deleted because we're going to implement redux state here
    
    BREAKING CHANGE: The only prop that was declared was type, and that variable is still in use on the
    Alert component. Also, I forgot to remove PropTypes from the import, so eslint will tell me that's
    an error

--- src/components/AlertMessage.jsx
+++ src/components/AlertMessage.jsx
@@ -3,14 +3,10 @@ import Alert from '@material-ui/lab/Alert';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const AlertMessage = ({ type }) => (
+const AlertMessage = () => (
   <Snackbar autoHideDuration={6000}>
     <Alert elevation={6} variant="filled" severity={type} />
   </Snackbar>
 );
-
-AlertMessage.propTypes = {
-  type: PropTypes.string.isRequired,
-};
 
 export default AlertMessage;
