07a0a3eadeb7c6d820ae9d346499a03398bdfb86
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-22 14:41:05 -0500

    fix(login route): separate error handling formuser and password validation
    The probllem was that if Mongoose didn't find any users, it returned null. Then with bcrypt, it
    would compare de given password with a null value, and validate later. So we split the two
    responses.

--- backend/routes/login.routes.js
+++ backend/routes/login.routes.js
@@ -6,13 +6,15 @@ const loginValidation = require('../validation/login.val');
 
 router.post('/', async (req, res) => {
   const { error } = loginValidation(req.body);
-  if (error) return res.status(400).send({ error: error.details[0].message });
+  if (error) return res.status(400).json({ error: error.details[0].message });
 
   const user = await User.findOne({ email: req.body.email });
-  const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!user || !validPassword) {
+  if (!user)
     return res.status(400).send({ error: 'Email or password is wrong' });
-  }
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword)
+    return res.status(400).send({ error: 'Email or password is wrong' });
 
   // eslint-disable-next-line no-underscore-dangle
   const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
