2f295537d1c7ad4d722b6894f9eaac6c3139ae1b
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-22 16:46:19 -0500

    refactor(database): move db logic and connection to database/index.js
    BREAKING CHANGE: On server.js, app is not defined, so it will break.

--- server.js
+++ server.js
@@ -1,39 +1,6 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
 const chalk = require('chalk');
-const cors = require('cors');
-const loginRoute = require('./routes/login.routes');
-const userRoute = require('./routes/user.routes');
-const electionRoute = require('./routes/election.routes');
 
 const { log } = console;
-
-// Environment variables configuration
-dotenv.config();
-
-mongoose.connect(
-  process.env.CONNECTION_STRING,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  () => log(chalk.blue('Connected to MongoDB Atlas!\n'))
-);
-
-const app = express();
-
-app.use(cors());
-
-// Configure express to recieve JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Route middleware.
-app.use('/api/login', loginRoute);
-// TODO: Check if the user is authenticated for the routes below.
-app.use('/api/users', userRoute);
-app.use('/api/elections', electionRoute);
 
 // Configure listening port
 app.listen(5000, () => log(chalk.blue('\nServer running!')));
