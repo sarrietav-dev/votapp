83af3a33543e2b9ccabda13dbcb083098ac92989
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-22 17:07:49 -0500

    feat(database): prepare mockgoose if server is in a testing environment

--- database/index.js
+++ database/index.js
@@ -9,16 +9,34 @@ dotenv.config();
 
 function connect() {
   return new Promise((resolve, reject) => {
-    mongoose
-      .connect(process.env.CONNECTION_STRING, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-      })
-      .then((res, err) => {
-        if (err) return reject(err);
-        log(chalk.blue('Connected to MongoDB Atlas!'));
-        resolve();
+    if (process.env.NODE_ENV === 'test') {
+      const Mockgoose = require('mockgoose').Mockgoose;
+      const mockgoose = new Mockgoose(mongoose);
+
+      mockgoose.prepareStorage().then(() => {
+        mongoose
+          .connect(process.env.CONNECTION_STRING, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+          })
+          .then((res, err) => {
+            if (err) return reject(err);
+            log(chalk.blue('Connected to MongoDB Atlas!'));
+            resolve();
+          });
       });
+    } else {
+      mongoose
+        .connect(process.env.CONNECTION_STRING, {
+          useNewUrlParser: true,
+          useCreateIndex: true,
+        })
+        .then((res, err) => {
+          if (err) return reject(err);
+          log(chalk.blue('Connected to MongoDB Atlas!'));
+          resolve();
+        });
+    }
   });
 }
 
