77bf9580406e4fb8e7571161cc928349ab4c5f25
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-22 16:48:45 -0500

    refactor(server.js): start server by listening to the port and connect to the database

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const chalk = require('chalk');
+const db = require('./database/index');
+const app = require('./app');
 
 const { log } = console;
 
-// Configure listening port
-app.listen(5000, () => log(chalk.blue('\nServer running!')));
+db.connect().then(() => {
+  app.listen(5000, () => log(chalk.blue('\nServer running!')));
+});
