a74e232fa6606e12f06675a57b5b74890e7d4fe9
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-24 21:19:28 -0500

    feat(user routes): create POST '/verify/:id' user route
    Its goal is to set the state of verify to users that want to join

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -60,4 +60,28 @@ router.get('/unverified', async (req, res) => {
   }
 });
 
+router.post('/verify/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findOne(
+      { _id: id },
+      { is_verified: 1, is_admin: 1 }
+    );
+
+    if (user.is_admin)
+      return res.status(400).json({ error: 'the user is an admin' });
+
+    if (user.is_verified)
+      return res.status(400).json({ error: 'The user is already verified' });
+
+    await User.updateOne({ _id: id }, { is_verified: true });
+    return res.status(200).json({ message: 'Operation completed' });
+  } catch (error) {
+    return res.status(400).send({ error });
+  }
+});
+
+
+
 module.exports = router;
