1808e5cd8836e4fe1b3c537424a1c9f98bce71bf
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-24 19:40:18 -0500

    refactor(routes): rename err variable to error to ensure consistency

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,13 +3,12 @@ const bcrypt = require('bcryptjs');
 const User = require('../database/models/User.model');
 const registerValidation = require('../validation/register.val');
 
-// TODO: Make get '/' route to get all the users.
 router.get('/', async (req, res) => {
   try {
     const users = await User.find({});
     return res.status(200).json(users);
-  } catch (err) {
-    return res.status(400).json({ error: err });
+  } catch (error) {
+    return res.status(400).json({ error });
   }
 });
 
@@ -17,35 +16,35 @@ router.get('/', async (req, res) => {
 
 // Create an user
 router.post('/', async (req, res) => {
-  // General validation
-  const { error } = registerValidation(req.body);
-  if (error) return res.status(400).send({ error: error.details[0].message });
-
-  // Email repeatition validation
-  const emailExist = await User.findOne({ email: req.body.email });
-  if (emailExist) return res.status(400).send({ error: 'Email exists' });
-
-  // Hash password
-  const salt = await bcrypt.genSalt();
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: hashedPassword,
-    birthdate: req.body.birthdate,
-    phoneNumber: req.body.phoneNumber,
-    gender: Boolean(req.body.gender),
-    is_admin: Boolean(req.body.is_admin),
-    code: req.body.code,
-  });
-
   try {
+    // General validation
+    const { error } = registerValidation(req.body);
+    if (error) return res.status(400).send({ error: error.details[0].message });
+
+    // Email repeatition validation
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) return res.status(400).send({ error: 'Email exists' });
+
+    // Hash password
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+      birthdate: req.body.birthdate,
+      phoneNumber: req.body.phoneNumber,
+      gender: Boolean(req.body.gender),
+      is_admin: Boolean(req.body.is_admin),
+      code: req.body.code,
+    });
+
     const savedUser = await user.save();
     // TODO: Delete user for security reasons.
     return res.status(200).send({ message: 'User created', savedUser });
-  } catch (err) {
-    return res.status(400).send({ error: err });
+  } catch (error) {
+    return res.status(400).send({ error });
   }
 });
 
@@ -56,8 +55,8 @@ router.get('/unverified', async (req, res) => {
       { name: 1, code: 1, email: 1, gender: 1 }
     );
     return res.status(200).json(unverifiedUsers);
-  } catch (err) {
-    return res.status(400).send({ error: err });
+  } catch (error) {
+    return res.status(400).send({ error });
   }
 });
 
