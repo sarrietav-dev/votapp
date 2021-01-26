a7d5aad00b775d4a2b988fa814c686868d8d1c13
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-25 23:00:58 -0500

    feat(thunks): create register thunk

---
+++ src/store/actions/thunks/register.thunks.js
@@ -0,0 +1,32 @@
+/* eslint-disable function-paren-newline */
+/* eslint-disable implicit-arrow-linebreak */
+import axios from 'axios';
+import { raiseAlert } from '../alert.actions';
+
+export default (data) => async (dispatch) => {
+  await axios({
+    method: 'POST',
+    url: 'http://localhost:3000/api/users/',
+    data,
+  })
+    .then(() => {
+      if (Object.prototype.hasOwnProperty.call(data, 'is_admin')) {
+        dispatch(
+          raiseAlert({
+            message: 'New admin has been created',
+            variant: 'success',
+          }),
+        );
+      } else {
+        dispatch(
+          raiseAlert({
+            message: 'Request sent!',
+            variant: 'success',
+          }),
+        );
+      }
+    })
+    .catch((error) =>
+      dispatch(raiseAlert({ message: error, variant: 'error' })),
+    );
+};
