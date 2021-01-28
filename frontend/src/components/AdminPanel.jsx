e2f5633ccbbabaebfae1853bcad181638153605b
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-28 15:56:16 -0500

    refactor(adminpanel): change close prop to dispatch closePanel action

--- frontend/src/components/AdminPanel.jsx
+++ frontend/src/components/AdminPanel.jsx
@@ -8,23 +8,27 @@ import {
 import AddUser from '@material-ui/icons/GroupAdd';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { closePanel } from '../store/actions/panel.actions';
 
-const AdminPanel = ({ open, close }) => (
-  <Drawer open={open} onClose={close}>
-    <List>
-      <ListItem>
-        <ListItemIcon>
-          <AddUser />
-        </ListItemIcon>
-        <ListItemText primary="Verify users" />
-      </ListItem>
-    </List>
-  </Drawer>
-);
+const AdminPanel = ({ open }) => {
+  const dispatch = useDispatch();
+  return (
+    <Drawer open={open} onClose={() => dispatch(closePanel())}>
+      <List>
+        <ListItem>
+          <ListItemIcon>
+            <AddUser />
+          </ListItemIcon>
+          <ListItemText primary="Verify users" />
+        </ListItem>
+      </List>
+    </Drawer>
+  );
+};
 
 AdminPanel.propTypes = {
   open: PropTypes.bool.isRequired,
-  close: PropTypes.func.isRequired,
 };
 
 export default AdminPanel;
