3ab0299698d10b9761cc819026f48a9c849e6666
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-28 18:57:20 -0500

    feat(redux reducer): create unverified user reducer and append it to the root reducer

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -4,6 +4,7 @@ import dialogReducer from './dialogs.reducer';
 import electionReducer from './elections.reducer';
 import alertReducer from './alerts.reducer';
 import panelReducer from './panel.reducer';
+import unverifiedUsersReducer from './unverified-users.reducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
@@ -11,6 +12,7 @@ const rootReducer = combineReducers({
   election: electionReducer,
   alert: alertReducer,
   panel: panelReducer,
+  unverified: unverifiedUsersReducer,
 });
 
 export default rootReducer;
