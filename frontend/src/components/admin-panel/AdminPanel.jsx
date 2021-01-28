416eb58fecaa3c921ec2065c7a6301799ff7c349
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-28 17:49:05 -0500

    feat(adminpanel): create handlers for admin dialogs

--- frontend/src/components/admin-panel/AdminPanel.jsx
+++ frontend/src/components/admin-panel/AdminPanel.jsx
@@ -6,24 +6,33 @@ import {
   ListItemText,
 } from '@material-ui/core';
 import AddUser from '@material-ui/icons/GroupAdd';
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { closePanel } from '../../store/actions/panel.actions';
+import VerifyUsersDialog from './VerifyUsersDialog';
 
 const AdminPanel = () => {
+  const [openDialog, setOpenDialog] = useState(false);
+
   const dispatch = useDispatch();
   const open = useSelector((state) => state.panel.open);
   return (
-    <Drawer open={open} anchor="right" onClose={() => dispatch(closePanel())}>
-      <List>
-        <ListItem button>
-          <ListItemIcon>
-            <AddUser />
-          </ListItemIcon>
-          <ListItemText primary="Verify users" />
-        </ListItem>
-      </List>
-    </Drawer>
+    <>
+      <Drawer open={open} anchor="right" onClose={() => dispatch(closePanel())}>
+        <List>
+          <ListItem button onClick={() => setOpenDialog(true)}>
+            <ListItemIcon>
+              <AddUser />
+            </ListItemIcon>
+            <ListItemText primary="Verify users" />
+          </ListItem>
+        </List>
+      </Drawer>
+      <VerifyUsersDialog
+        isOpen={openDialog}
+        onClose={() => setOpenDialog(false)}
+      />
+    </>
   );
 };
 
