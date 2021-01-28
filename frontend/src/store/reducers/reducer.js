168887d3ae305c4daabbda57ce2dc772fa638e34
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-28 15:51:00 -0500

    feat(root reducer): add panel reucer to the root reducer

--- frontend/src/store/reducers/reducer.js
+++ frontend/src/store/reducers/reducer.js
@@ -3,12 +3,14 @@ import authReducer from './auth-token.reducer';
 import dialogReducer from './dialogs.reducer';
 import electionReducer from './elections.reducer';
 import alertReducer from './alerts.reducer';
+import panelReducer from './panel.reducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   dialog: dialogReducer,
   election: electionReducer,
   alert: alertReducer,
+  panel: panelReducer,
 });
 
 export default rootReducer;
