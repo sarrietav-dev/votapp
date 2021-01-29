ce700a2f1d933ad8788094d3340f8345d30daacb
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-28 21:02:08 -0500

    feat(redux reducers): add deny_user type to the unverified-users reducer

--- src/store/reducers/unverified-users.reducer.js
+++ src/store/reducers/unverified-users.reducer.js
@@ -1,5 +1,9 @@
 import { deleteOne } from '../../utils/reducer.utils';
-import { FETCH_UNVERIFIED_USERS, VERIFY_USER } from '../actions/types.actions';
+import {
+  DENY_USER,
+  FETCH_UNVERIFIED_USERS,
+  VERIFY_USER,
+} from '../actions/types.actions';
 
 const initialState = {
   unverifiedUsers: [],
@@ -9,7 +13,7 @@ export default (state = initialState, { type, payload }) => {
   switch (type) {
     case FETCH_UNVERIFIED_USERS:
       return { unverifiedUsers: payload };
-    case VERIFY_USER:
+    case VERIFY_USER || DENY_USER:
       return {
         ...state,
         unverifiedUsers: deleteOne(state.unverifiedUsers, payload),
