fe33b85c974e90c8a04c4d70aa58425f622ab935
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-28 20:52:16 -0500

    feat(verify users dialog): fetch the unverified users and render them

--- frontend/src/components/admin-panel/VerifyUsersDialog.jsx
+++ frontend/src/components/admin-panel/VerifyUsersDialog.jsx
@@ -1,19 +1,31 @@
 import { Dialog, DialogContent } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { generate } from 'shortid';
 import UnverifiedUser from './UnverifiedUser';
+import { fetchUnverifiedUsersThunk } from '../../store/actions/thunks/verify.thunks';
 
-const VerifyUsersDialog = ({ isOpen, onClose }) => (
-  <Dialog open={isOpen} onClose={onClose} fullWidth>
-    <DialogContent>
-      <UnverifiedUser
-        data={{
-          name: 'Sebastian',
-        }}
-      />
-    </DialogContent>
-  </Dialog>
-);
+const VerifyUsersDialog = ({ isOpen, onClose }) => {
+  const unverifiedUsers = useSelector(
+    (state) => state.unverified.unverifiedUsers,
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUnverifiedUsersThunk());
+  }, []);
+
+  return (
+    <Dialog open={isOpen} onClose={onClose} fullWidth>
+      <DialogContent>
+        {unverifiedUsers.map((user) => (
+          <UnverifiedUser data={user} key={generate()} />
+        ))}
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 VerifyUsersDialog.propTypes = {
   isOpen: PropTypes.bool.isRequired,
