042079cd7886e6b19827f3dcdd0d5af5e168c698
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-28 21:28:39 -0500

    feat(admin panel): raise warning if there aren't users to check

--- frontend/src/components/admin-panel/AdminPanel.jsx
+++ frontend/src/components/admin-panel/AdminPanel.jsx
@@ -8,6 +8,7 @@ import {
 import AddUser from '@material-ui/icons/GroupAdd';
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { raiseAlert } from '../../store/actions/alert.actions';
 import { closePanel } from '../../store/actions/panel.actions';
 import VerifyUsersDialog from './VerifyUsersDialog';
 
@@ -16,11 +17,29 @@ const AdminPanel = () => {
 
   const dispatch = useDispatch();
   const open = useSelector((state) => state.panel.open);
+
+  const unverifiedUsersLength = useSelector(
+    (state) => state.unverified.unverifiedUsers.length,
+  );
+
+  const handleAddUserClick = () => {
+    if (unverifiedUsersLength === 0) {
+      dispatch(
+        raiseAlert({
+          message: 'There are no users left to check',
+          variant: 'warning',
+        }),
+      );
+    } else {
+      dispatch(closePanel());
+    }
+  };
+
   return (
     <>
       <Drawer open={open} anchor="right" onClose={() => dispatch(closePanel())}>
         <List>
-          <ListItem button onClick={() => setOpenDialog(true)}>
+          <ListItem button onClick={handleAddUserClick}>
             <ListItemIcon>
               <AddUser />
             </ListItemIcon>
