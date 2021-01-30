923d868d57ba53d44e3999ce0099f80ec88f2712
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-30 11:19:52 -0500

    fix(store.js): delete enhancers

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,16 +1,8 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-underscore-dangle */
-import { applyMiddleware } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
 import rootReducer from './reducers/reducer';
-
-const enhancers = [
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-];
 
 export default configureStore({
   reducer: rootReducer,
-  enhancers,
 });
