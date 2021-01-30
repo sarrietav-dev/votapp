1c4999b6ec571876c5e67bc3bb6a771101aac548
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-30 13:02:27 -0500

    feat(user selector): render autocomplete component from material-ui
    Get the users from the redux store, and when rendered, fetch the users

--- frontend/src/components/dashboard/UserSelector.jsx
+++ frontend/src/components/dashboard/UserSelector.jsx
@@ -1,5 +1,43 @@
-import React from 'react';
+/* eslint-disable react/jsx-curly-newline */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { Chip, TextField } from '@material-ui/core';
+import fetchUsers from '../../store/thunks/user.thunks';
 
-const UserSelector = () => <div />;
+const UserSelector = () => {
+  const users = useSelector((state) => state.users.users);
+  const dispatch = useDispatch();
+
+  useEffect(() => dispatch(fetchUsers()), []);
+
+  return (
+    <div className="">
+      <Autocomplete
+        multiple
+        options={users}
+        getOptionLabel={(option) => option.name}
+        renderTags={(value, getTagProps) =>
+          value.map((option, index) => (
+            <Chip
+              variant="default"
+              label={option}
+              {...getTagProps({ index })}
+            />
+          ))
+        }
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="outlined"
+            placeholder="Select the candidates"
+          />
+        )}
+      />
+    </div>
+  );
+};
 
 export default UserSelector;
