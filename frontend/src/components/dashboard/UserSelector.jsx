172b6bb91de773f519a3f9b37add79c61068fad4
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-01-30 22:19:49 -0500

    fix(user selector): fix error on Chip component
    The label asks for a string and I provided it an object

--- frontend/src/components/dashboard/UserSelector.jsx
+++ frontend/src/components/dashboard/UserSelector.jsx
@@ -1,42 +1,37 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable react/jsx-curly-newline */
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { Chip, TextField } from '@material-ui/core';
-import fetchUsers from '../../store/thunks/user.thunks';
 
 const UserSelector = () => {
   const users = useSelector((state) => state.users.users);
-  const dispatch = useDispatch();
-
-  useEffect(() => dispatch(fetchUsers()), []);
 
   return (
-    <div className="">
-      <Autocomplete
-        multiple
-        options={users}
-        getOptionLabel={(option) => option.name}
-        renderTags={(value, getTagProps) =>
-          value.map((option, index) => (
-            <Chip
-              variant="default"
-              label={option}
-              {...getTagProps({ index })}
-            />
-          ))
-        }
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            variant="outlined"
-            placeholder="Select the candidates"
+    <Autocomplete
+      multiple
+      options={users}
+      getOptionLabel={(option) => option.name}
+      renderTags={(value, getTagProps) =>
+        value.map((option, index) => (
+          <Chip
+            variant="default"
+            label={option.name}
+            {...getTagProps({ index })}
           />
-        )}
-      />
-    </div>
+        ))
+      }
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          variant="outlined"
+          placeholder="Select the candidates"
+        />
+      )}
+    />
   );
 };
 
