4375aab5d765517ddbf32cd52e31b4656c8508b1
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-02-01 17:13:25 -0500

    feat(candidate card): render vote icon and increment the card's width

--- frontend/src/components/election/CandidateCard.jsx
+++ frontend/src/components/election/CandidateCard.jsx
@@ -3,18 +3,32 @@ import PropTypes from 'prop-types';
 import {
   Avatar,
   Card,
+  CardActions,
   CardContent,
   CardHeader,
+  IconButton,
   Typography,
 } from '@material-ui/core';
+import VoteIcon from '@material-ui/icons/HowToVote';
 
 const CandidateCard = ({ data, onClick }) => (
-  <Card onClick={onClick} style={{ maxWidth: 500 }}>
+  <Card onClick={onClick} style={{ maxWidth: 500, width: 250 }}>
     <CardHeader avatar={<Avatar>{data.name[0]}</Avatar>} title="Candidate" />
     <CardContent>
       <Typography>{data.name}</Typography>
       <Typography>{data.code}</Typography>
     </CardContent>
+    <CardActions
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <IconButton>
+        <VoteIcon />
+      </IconButton>
+    </CardActions>
   </Card>
 );
 
