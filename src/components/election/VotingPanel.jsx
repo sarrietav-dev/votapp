62e47fff033fcd51098a1a8f5f7b1227e830c3fc
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-02-01 17:11:35 -0500

    feat(voting panel): render blank ballot as CandidateCard

--- src/components/election/VotingPanel.jsx
+++ src/components/election/VotingPanel.jsx
@@ -1,7 +1,10 @@
-import { makeStyles } from '@material-ui/core';
-import React from 'react';
+/* eslint-disable operator-linebreak */
+import { IconButton, makeStyles } from '@material-ui/core';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { generate } from 'shortid';
+import BackIcon from '@material-ui/icons/ArrowBack';
+import { useHistory } from 'react-router';
 import CandidateCard from './CandidateCard';
 
 const useStyles = makeStyles(() => ({
@@ -14,22 +17,21 @@ const useStyles = makeStyles(() => ({
 }));
 
 const VotingPanel = () => {
-  const candidates = useSelector(
-    (state) => state.election.currentElection.candidates,
-  );
+  const candidates =
+    useSelector((state) => state.election.currentElection.candidates) || [];
   const classes = useStyles();
+  const history = useHistory();
 
-  const onClick = (id) => {
-    console.log(id);
-  };
 
   return (
-    <div className={classes.votingPanel}>
-      {candidates.map((candidate) => (
-        <CandidateCard data={candidate} onClick={onClick} key={generate()} />
-      ))}
-      <CandidateCard data={{ name: 'Blank ballot', code: '' }} />
-    </div>
+    <>
+      <div className={classes.votingPanel}>
+        {candidates.map((candidate) => (
+          <CandidateCard data={candidate} onClick={onClick} key={generate()} />
+        ))}
+        <CandidateCard data={{ name: 'Blank ballot', code: '' }} />
+      </div>
+    </>
   );
 };
 
