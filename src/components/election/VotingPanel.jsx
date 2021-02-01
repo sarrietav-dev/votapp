a63cf0a93c9a302cda0cbe42af9fb542e586973b
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2021-02-01 14:14:33 -0500

    feat(voting panel): add styles and render candidates from the selected election

--- src/components/election/VotingPanel.jsx
+++ src/components/election/VotingPanel.jsx
@@ -1,5 +1,27 @@
+import { makeStyles } from '@material-ui/core';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { generate } from 'shortid';
 
-const VotingPanel = () => <div />;
+const useStyles = makeStyles(() => ({
+  votingPanel: {
+    height: '100vh',
+  },
+}));
+
+const VotingPanel = () => {
+  const candidates = useSelector(
+    (state) => state.election.currentElection.candidates,
+  );
+  const classes = useStyles();
+
+  return (
+    <div className={classes.votingPanel}>
+      {candidates.map((candidate) => (
+        <h1 key={generate()}>{candidate}</h1>
+      ))}
+    </div>
+  );
+};
 
 export default VotingPanel;
