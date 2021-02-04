89d8cf1ca2af942c5f7814fe4990a4c27f85ca21
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-02-03 23:09:29 -0500

    fix(auth reducer): fix autologin not filling isAdmin state property

--- frontend/src/store/reducers/auth-token.reducer.js
+++ frontend/src/store/reducers/auth-token.reducer.js
@@ -1,15 +1,21 @@
+/* eslint-disable operator-linebreak */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 import { createSlice } from '@reduxjs/toolkit';
+import { verify } from 'jsonwebtoken';
+
+const authToken = localStorage.getItem('AUTH_TOKEN') || '';
+const tokenData =
+  authToken !== '' ? verify(authToken, process.env.REACT_APP_TOKEN_SECRET) : '';
 
 const authSlice = createSlice({
   name: 'auth',
   initialState: {
-    authToken: localStorage.getItem('AUTH_TOKEN') || '',
-    isAdmin: false,
-    _id: '',
+    authToken,
+    isAdmin: tokenData.is_admin,
+    _id: tokenData._id,
   },
   reducers: {
     setAuthToken(state, action) {
