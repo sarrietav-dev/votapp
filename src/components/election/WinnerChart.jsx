3cc41a8904ce0871a23d2a7fcb711010f331c777
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-02-05 23:15:00 -0500

    feat(winner chart): return a Bar chart from chart.js

--- src/components/election/WinnerChart.jsx
+++ src/components/election/WinnerChart.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Bar } from '@reactchartjs/react-chart.js';
+import parseData from './utils/parse-data';
 
-const WinnerChart = () => <div />;
+const WinnerChart = () => {
+  const candidates = useSelector(
+    (state) => state.election.currentElection.candidates,
+  );
+
+  return <Bar data={parseData(candidates)} />;
+};
 
 export default WinnerChart;
