f1931791315999e060c573772bbb0420058b88ac
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-02-05 23:37:30 -0500

    fix(election routes): add _id, code and email properties to the blank ballot

--- backend/routes/election.routes.js
+++ backend/routes/election.routes.js
@@ -19,7 +19,10 @@ router.post('/', async (req, res) => {
     }));
 
     candidates.push({
+      _id: 'nzw3N5Y57Qv6vw',
       name: 'Blank ballot',
+      code: '',
+      email: 'blank@ballot.com',
     });
 
     // TODO: Check if the requester is an admin.
