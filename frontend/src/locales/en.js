6a888d75858ad8ba4003237cbd1ce4ef3e6cd073
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-02-06 18:01:24 -0500

    feat(locales): add few locales for 4 components

---
+++ frontend/src/locales/en.js
@@ -0,0 +1,28 @@
+export default () => ({
+  login: {
+    password: 'Password',
+    logIn: 'Log In',
+    requestAccess: 'Request access',
+  },
+  registerDialog: {
+    yourName: 'Your Name',
+    code: 'Code',
+    password: 'Password',
+    gender: 'Gender',
+    male: 'Male',
+    female: 'Female',
+    cancel: 'Cancel',
+    send: 'Send',
+  },
+  dashboard: {
+    blob: 'There are not elections yet',
+  },
+  createElectionDialog: {
+    createElection: 'Create election',
+    title: 'Title',
+    position: 'Position',
+    selectCandidates: 'Select Candidates Beforehand',
+    cancel: 'Cancel',
+    create: 'Create',
+  },
+});
---
+++ frontend/src/locales/es.js
@@ -0,0 +1,28 @@
+export default () => ({
+  login: {
+    password: 'Contraseña',
+    logIn: 'Ingresar',
+    requestAccess: 'Solicitar acceso',
+  },
+  registerDialog: {
+    yourName: 'Tu nombre',
+    code: 'Código',
+    password: 'Contraseña',
+    gender: 'Género',
+    male: 'Hombre',
+    female: 'Mujer',
+    cancel: 'Cancelar',
+    send: 'Enviar',
+  },
+  dashboard: {
+    blob: 'No hay elecciones.',
+  },
+  createElectionDialog: {
+    createElection: 'Crear elección',
+    title: 'Titulo',
+    position: 'Posición',
+    selectCandidates: 'Seleccionar candidatos de antemano',
+    cancel: 'Cancelar',
+    create: 'Crear',
+  },
+});
