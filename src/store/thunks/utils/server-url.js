be176335e8583b35d28643bb8b5df361879fc5da
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-02-07 21:21:31 -0500

    test(server-url): commented other url and leave production url

--- src/store/thunks/utils/server-url.js
+++ src/store/thunks/utils/server-url.js
@@ -1,3 +1,4 @@
-export default process.env.NODE_ENV === 'development'
-  ? 'http://localhost:5000/api'
-  : 'https://votapp-backend.herokuapp.com/api';
+export default 'https://votapp-backend.herokuapp.com/api';
+// export default process.env.NODE_ENV === 'development'
+// ? 'http://localhost:5000/api'
+// : 'https://votapp-backend.herokuapp.com/api';
