deda79e0415492fb669424e8902b82994c085cf1
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-02-08 11:25:25 -0500

    fix(locale select): use locales reducer's current language on the LocaleSelect component

--- src/components/admin-panel/LocaleSelect.jsx
+++ src/components/admin-panel/LocaleSelect.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import { InputLabel, MenuItem, Select } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setLocale } from '../../store/reducers/locales.reducer';
 
 const LocaleSelect = () => {
   const dispatch = useDispatch();
+  const currentLanguage = useSelector((state) => state.locales.language);
   const onChangeHandler = (e) => {
     dispatch(setLocale(e.target.value));
   };
   return (
     <FormControl style={{ padding: 10 }}>
       <InputLabel>Language</InputLabel>
-      <Select onChange={onChangeHandler} defaultValue="en">
+      <Select onChange={onChangeHandler} defaultValue={currentLanguage}>
         <MenuItem value="en">English</MenuItem>
         <MenuItem value="es">Español</MenuItem>
       </Select>
